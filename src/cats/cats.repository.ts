eaceef321696d5d07508bca60a333822c3fc7632
Gimoring <62681451+Gimoring@users.noreply.github.com>
2021-08-02 15:33:22 +0900

    add Repository Pattern

---
+++ src/cats/cats.repository.ts
@@ -0,0 +1,21 @@
+import { Injectable, HttpException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/cats.request.dto';
+
+// 얘도 D.I 가 가능한 클래스임.
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  // 이메일을 인자로 받아 데이터베이스에 있는지 체크.
+  async existsByEmail(email: string): Promise<boolean> {
+    const result = await this.catModel.exists({ email });
+    return result;
+  }
+
+  async create(cat: CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,3 +1,4 @@
+import { CatsRepository } from './cats.repository';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -8,11 +9,13 @@ import { CatRequestDto } from './dto/cats.request.dto';
 @Injectable()
 export class CatsService {
   // 스키마 사용 Dependency Injection
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  // constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  constructor(private readonly catsRepository: CatsRepository) {}
 
   async signUp(body: CatRequestDto) {
     const { email, name, password } = body;
-    const isCatExist = await this.catModel.exists({ email }); // email이 존재하는지?
+    const isCatExist = await this.catsRepository.existsByEmail(email); // email이 존재하는지?
 
     // 유효성 검사
     if (isCatExist) {
@@ -22,7 +25,7 @@ export class CatsService {
 
     // 패스워드 암호화
     const hashedPassword = await bcrypt.hash(password, 10);
-    const cat = await this.catModel.create({
+    const cat = await this.catsRepository.create({
       email,
       name,
       password: hashedPassword,
